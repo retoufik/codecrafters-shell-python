132cf0978a352f16d05c0cfd0cf896071617910b
retoufik <wachani3@outlook.com>
2025-11-11 16:46:51 +0100

    exit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import sys
-
+import string
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
         command = input()
-        print(f"{command}: command not found")
+        if command.strip().lower() != "exit 0":
+            print(f"{command}: command not found")
+            continue
+        break
+
 
 if __name__ == "__main__":
     main()
