38184b2989e7f1978262840e258289892f23345d
retoufik <wachani3@outlook.com>
2025-11-11 16:35:04 +0100

    Ã

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    command = input()
-    print(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
