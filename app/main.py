c7b3a4b15c9b383f6b4780c4ddc79e3507a20114
retoufik <wachani3@outlook.com>
2025-11-11 16:30:16 +0100

    stageCZ2F

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     command = input()
-    print(f"{command}: command does not exist, Try again")
+    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
