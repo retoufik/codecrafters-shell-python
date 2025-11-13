97c2e75d0f79be43678103e6fb41acf9af93ea8c
retoufik <wachani3@outlook.com>
2025-11-13 16:35:57 +0100

    type

--- app/main.py
+++ app/main.py
@@ -7,9 +7,16 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         if command.strip().lower() != "exit 0":
-            if command.strip().lower().split()[0]== "echo":
+            if command.strip().lower().split()[0] == "echo":
                     args = command.strip().split()[1:]
                     print(" ".join(args))
+            elif command.strip().lower().split()[0] == "type":
+                    args = command.strip().split()[1:]
+                    for arg in args:
+                        if arg.lower() in ["echo", "type", "exit"]:
+                            print(f"{arg} is a shell builtin")
+                        else:
+                            print(f"{arg}: not found")
             else:
                 sys.stdout.write(f"{command}: command not found\n")
             continue
